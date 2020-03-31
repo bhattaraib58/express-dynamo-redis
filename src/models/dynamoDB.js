34d05a5a5fc28fe6fbcd94532539e31c8f8e5930
Biplap Bhattarai <bhattaraib58@gmail.com>
2020-03-31 17:05:07 +0545

    Update Dynamo DB model to allow production database

--- src/models/dynamoDB.js
+++ src/models/dynamoDB.js
@@ -1,10 +1,21 @@
 import AWS from 'aws-sdk';
 import config from '../config';
+import logger from '../utils/logger';
 
-AWS.config.update({
-  region: config.dynamoDB.region,
-  endpoint: `http://${config.dynamoDB.host}:${config.dynamoDB.port}`
-});
+if (config.environment.isProduction) {
+  logger.info(`Using Production Database at: https://dynamodb.${config.dynamoDB.region}.amazonaws.com`);
+
+  AWS.config.update({
+    endpoint: `https://dynamodb.${config.dynamoDB.region}.amazonaws.com`
+  });
+} else {
+  logger.info(`Using Development Database at: http://${config.dynamoDB.host}:${config.dynamoDB.port}`);
+
+  AWS.config.update({
+    region: config.dynamoDB.region,
+    endpoint: `http://${config.dynamoDB.host}:${config.dynamoDB.port}`
+  });
+}
 
 export const dynamoDB = new AWS.DynamoDB();
 
